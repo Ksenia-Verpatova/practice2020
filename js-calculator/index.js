26ab2763e691bbd6cad167c740d0001bd49c5f2a
Alexander Lee <alxnder.lee@gmail.com>
2020-07-05 21:51:40 +0600

    Add new operations for calculator Operations: - exponentiation - root extraction - sin, cos, tg, ctg calculation - percentage of a number Handle incorrect operation exception

--- js-calculator/index.js
+++ js-calculator/index.js
@@ -19,8 +19,53 @@ app.post("/", function(request, response) {
         case '/':
             result = arg1 / arg2;
             break;
+        default:
+            result = "Ошибка: неверная операция.";
     }
     response.status(200).json(result);
+});
+
+app.post("/power", function(request, response) {
+    const { arg, power, oper } = request.body;
+    let result;
+    switch (oper) {
+        case 'pow':
+            result = arg ** power;
+            break;
+        case 'root':
+            result = arg ** (1/power);
+            break;
+        default:
+            result = "Ошибка: неверная операция.";
+    }
+    response.status(200).json(result);
+});
+
+app.post("/trigonom", function(request, response) {
+    const { arg, oper } = request.body;
+    let result;
+    switch (oper) {
+        case 'cos':
+            result = Math.cos(arg);
+            break;
+        case 'sin':
+            result = Math.sin(arg);
+            break;
+        case 'tg':
+            result = Math.tan(arg);
+            break;
+        case 'ctg':
+            result = 1 / Math.tan(arg);
+            break;
+        default:
+            result = "Ошибка: неверная операция.";
+    }
+    response.status(200).json(result);
+});
+
+app.post("/percent", function(request, response) {
+    const { arg, percent } = request.body;
+    response.status(200).json(arg * percent / 100);
 });
 
 app.listen(2121);
